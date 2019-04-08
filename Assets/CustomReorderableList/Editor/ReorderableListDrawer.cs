53b0afc902f96b447aa0a86effef670b2065ed04
Hideki_Abe <abe_h@cs-reporters.com>
2019-04-08 10:31:30 +0900

    折りたたみ実装

--- Assets/CustomReorderableList/Editor/ReorderableListDrawer.cs
+++ Assets/CustomReorderableList/Editor/ReorderableListDrawer.cs
@@ -18,6 +18,8 @@ public class ReorderableListDrawer : PropertyDrawer
     private ReorderableList reorder;
     private SerializedProperty prop;
     private bool callbackAdd = false;
+    private float height;
+    private bool foldout = false;
 
     // 描画
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
@@ -25,6 +27,10 @@ public class ReorderableListDrawer : PropertyDrawer
         if (prop == null) prop = property;
         if (prop != null)
         {
+            position.height = height;
+            // 折りたたみ切り替え
+            foldout = EditorGUI.Foldout(position, foldout, prop.displayName);
+            // リスト作成
             reorder = GetList(prop.FindPropertyRelative("m_list"));
             if (!callbackAdd)
             {
@@ -32,17 +38,23 @@ public class ReorderableListDrawer : PropertyDrawer
                 reorder.drawElementCallback += DrawElement;
                 callbackAdd = true;
             }
-            prop.serializedObject.Update();
-            reorder.DoList(position);
-            prop.serializedObject.ApplyModifiedProperties();
+            // 折りたたみ分岐
+            if (foldout)
+            {
+                prop.serializedObject.Update();
+                position.y = position.y + height;
+                reorder.DoList(position);
+                prop.serializedObject.ApplyModifiedProperties();
+            }
         }
     }
 
     // 描画領域
 	public override float GetPropertyHeight (SerializedProperty property, GUIContent label)
     {
+        height = base.GetPropertyHeight(property, label);
         var fieldProp = property.FindPropertyRelative("m_list");
-        return GetList(fieldProp).GetHeight();
+        return foldout ? height + GetList(fieldProp).GetHeight() : height;
     }
 
 	// リストを取得する
@@ -58,7 +70,7 @@ public class ReorderableListDrawer : PropertyDrawer
 	// ヘッダーの描画
 	private void DrawHeader (Rect rect)
 	{
-        EditorGUI.LabelField(rect, prop.displayName);
+        EditorGUI.LabelField(rect, "Size: " + prop.FindPropertyRelative("m_list").arraySize);
     }
 
 	// 要素の描画
